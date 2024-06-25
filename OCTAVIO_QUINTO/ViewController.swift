7e872eb790d9c1853c73ec44128dbc8f3795474f
fernandom11399 <139576221+fernandom11399@users.noreply.github.com>
2024-06-25 12:35:29 -0600

    Primer commit

---
+++ OCTAVIO_QUINTO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCTAVIO_QUINTO
+//
+//  Created by Federico Mireles on 18/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
